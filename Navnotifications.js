eebfda7f8709ce2f7a20ddfb4b3e2c50965d2a56
Jiiks <jiiks@windowslive.com>
2015-11-22 12:20:19 +0200

    css
    css

--- Navnotifications.js
+++ Navnotifications.js
@@ -12,7 +12,7 @@
     
     $("<style type='text/css'> .jcounter { background-color:#9C393D; display:inline-block; height:15px; min-width:15px; text-align:center; font-size:10px; line-height:15px; color:#FFF; border-radius:2px; } .jpadder { display:inline-block; height:15px; width:15px; } </style>").appendTo("head");
     $("<style type='text/css'> .jncontainer { position:absolute; right:48px; height:38px; display:inline-block; top:27px; } .fixed-scroll-breakpoint .jncontainer { top:8px; } </style>").appendTo("head");
-    $("<style type='text/css'> .jlink { font-size:12px; display:block !important; height:18px !important; color:#FFF !important; text-align:right !important; } .search-bar-outer { right:165px !important; } </style>").appendTo("head");
+    $("<style type='text/css'> .jlink { font-size:12px; display:block !important; height:18px !important; color:#FFF !important; text-align:right !important; } .search-bar-outer, .ui-header-fixed ul#main-nav .search > a { right:165px !important; } .ui-header-fixed li.profile .user-avatar .notification-counter, .ui-header-fixed ul#main-nav li.messages, .ui-header-fixed ul#main-nav li.subscriptions { display:none; } </style>").appendTo("head");
     
     var privateMessagesCount = $(".messages .notification-counter").first().text().replace(/\s+/, "");
     var subscriptionsCount = $(".subscriptions .notification-counter").first().text().replace(/\s+/, "");
