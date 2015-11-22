61e7396f1c7fe88fc9e301573803b045b1a01e58
Jiiks <jiiks@windowslive.com>
2015-11-22 08:21:56 +0200

    Nav notifications
    Add subscription/pm notifications to navbar

---
+++ Navnotifications.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name        Notification Restore
+// @namespace   nope
+// @include     http://www.overclock.net/*
+// @version     1
+// @grant       none
+// @require     http://code.jquery.com/jquery-1.6.4.js
+// ==/UserScript==
+
+(function() {
+    
+    
+    $("<style type='text/css'> .jcounter { background-color:#9C393D; display:inline-block; height:15px; min-width:15px; text-align:center; font-size:10px; line-height:15px; color:#FFF; border-radius:2px; } .jpadder { display:inline-block; height:15px; width:15px; } </style>").appendTo("head");
+    $("<style type='text/css'> .jncontainer { position:absolute; right:48px; height:38px; display:inline-block; top:27px; } .fixed-scroll-breakpoint .jncontainer { top:8px; } </style>").appendTo("head");
+    $("<style type='text/css'> .jlink { font-size:12px; display:block !important; height:18px !important; color:#FFF !important; text-align:right !important; } .search-bar-outer { right:165px !important; } </style>").appendTo("head");
+    
+    var privateMessagesCount = $(".messages .notification-counter").first().text().replace(/\s+/, "");
+    var subscriptionsCount = $(".subscriptions .notification-counter").first().text().replace(/\s+/, "");
+        
+    var notificationModule  = $("<li/>", {
+        class: "jncontainer",
+        css: {
+            "height": "35px"
+        }
+    });
+    
+    var messagesContainer = $("<a/>", {
+        href: "http://www.overclock.net/messages",
+        text: "Private Messages ",
+        class: "jlink",
+        css: {
+            "display": "block"
+        }
+    });
+    
+    var messagesCounter = $("<span/>", {
+        text: privateMessagesCount,
+        class: "jcounter"
+    });
+    
+    var subsContainer = $("<a/>", {
+        href: "http://www.overclock.net/users/subscriptions/",
+        text: "Subscriptions ",
+        class: "jlink",
+        css: {
+            "display": "block"
+        }
+    });
+    
+    var subsCounter = $("<span/>", {
+        text: subscriptionsCount,
+        class: "jcounter"
+    });
+    
+    
+    if(privateMessagesCount > 0) messagesContainer.append(messagesCounter); else messagesContainer.append($("<span/>", { class:"jpadder" }));
+    if(subscriptionsCount > 0) subsContainer.append(subsCounter); else subscriptionsCount.append($("<span/>", { class:"jpadder" }));
+    
+    notificationModule.append(messagesContainer);
+    notificationModule.append(subsContainer);
+    notificationModule.insertBefore($("#main-nav .profile"));
+
+})();
